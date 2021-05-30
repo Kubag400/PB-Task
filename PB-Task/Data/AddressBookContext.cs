4be17c2e02d3b5c7e539575d569a9c1310ebc938
Jakub Gawęda <kubag98@hotmail.com>
2021-05-30 17:47:48 +0200

    Create Data folder with AddressBookContext - sqlite db preparation

---
+++ PB-Task/Data/AddressBookContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PB_Task.Data
+{
+    public class AddressBookContext : DbContext
+    {
+        public AddressBookContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
