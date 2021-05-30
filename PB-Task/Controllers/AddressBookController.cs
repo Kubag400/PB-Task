812fcffa6b5393a3285b657fb32b289c48fad78e
Jakub Gawęda <kubag98@hotmail.com>
2021-05-30 23:38:55 +0200

    fixes and refactoring

--- PB-Task/Controllers/AddressBookController.cs
+++ PB-Task/Controllers/AddressBookController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using PB_Task.Helpers;
 using PB_Task.Interfaces;
 using PB_Task.Models;
@@ -11,44 +12,60 @@ namespace PB_Task.Controllers
     public class AddressBookController : Controller
     {
         private readonly IAddressBookRepository _repository;
-        public AddressBookController(IAddressBookRepository repository)
+        private readonly ILogger<AddressBookController> _logger;
+        public AddressBookController(IAddressBookRepository repository, ILogger<AddressBookController> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
         [HttpPost(ApiRoutes.AddressBook.Add)]
         public async Task<IActionResult> InsertAddressToBook([FromBody] AddressToAdd address)
         {
+            _logger.LogInformation("InsertAddressToBook invoked...");
             if (ModelState.IsValid)
             {
                 bool insert = await _repository.InsertToAddressBookAsync(address);
                 if (insert)
                 {
+                    _logger.LogInformation("Successfully!");
                     return Ok();
                 }
+                _logger.LogError("An error has occured - phone number already exists");
                 return Conflict("Phone number already exists!");
             }
+            _logger.LogError("An error has occured - insert value correctly");
             return BadRequest("Insert value correctly");
         }
         [HttpGet(ApiRoutes.AddressBook.FindByCity)]
         public async Task<ActionResult<List<AddressDb>>> FindByCity(string city)
         {
+            _logger.LogInformation("FindByCity invoked...");
             if (!string.IsNullOrEmpty(city))
             {
-                return await _repository.FindByCityAsync(city);
+                var result=await _repository.FindByCityAsync(city);
+                if(result.Count>0)
+                {
+                    _logger.LogInformation("Sucessfully!");
+                    return result;
+                }
             }
-            return NotFound() ;
+            _logger.LogError("An error has occured - city not found");
+            return NotFound("City not found!");
         }
         [HttpGet(ApiRoutes.AddressBook.GetLastAddress)]
         public async Task<ActionResult<AddressDb>> GetLastAddress()
         {
+            _logger.LogInformation("GetLastAddress invoked...");
             var lastAddress = await _repository.GetLastAddress();
-            if(lastAddress!=null)
+            if (lastAddress != null)
             {
+                _logger.LogInformation("Sucessfully!");
                 return lastAddress;
             }
-            return NotFound();
+            _logger.LogError("An error has occured - db is empty");
+            return NotFound("Db is empty!");
         }
-     
+
     }
 }
 
--- PB-Task/Services/AddressBookRepository.cs
+++ PB-Task/Services/AddressBookRepository.cs
@@ -2,7 +2,6 @@
 using PB_Task.Data;
 using PB_Task.Interfaces;
 using PB_Task.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
