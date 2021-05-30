99af8367b865f0fdfe7555c92a586341b456c4bc
Jakub Gawęda <kubag98@hotmail.com>
2021-05-30 18:41:38 +0200

    Create AddressBookRepository

---
+++ PB-Task/Services/AddressBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using PB_Task.Interfaces;
+using PB_Task.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PB_Task.Services
+{
+    public class AddressBookRepository : IAddressBookRepository
+    {
+        public Task<bool> InsertToAddressBookAsync(Address address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
