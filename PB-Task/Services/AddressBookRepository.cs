79b9bc9f5d0da5e91f61efbeec89ea6b1a173c88
Jakub Gawęda <kubag98@hotmail.com>
2021-05-30 18:45:53 +0200

    Create constructor in AddressBookRepository with AddressBookContext

--- PB-Task/Services/AddressBookRepository.cs
+++ PB-Task/Services/AddressBookRepository.cs
@@ -1,4 +1,5 @@
-﻿using PB_Task.Interfaces;
+﻿using PB_Task.Data;
+using PB_Task.Interfaces;
 using PB_Task.Models;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,11 @@ namespace PB_Task.Services
 {
     public class AddressBookRepository : IAddressBookRepository
     {
+        private readonly AddressBookContext _context;
+        public AddressBookRepository(AddressBookContext context)
+        {
+            _context = context;
+        }
         public Task<bool> InsertToAddressBookAsync(Address address)
         {
             throw new NotImplementedException();
